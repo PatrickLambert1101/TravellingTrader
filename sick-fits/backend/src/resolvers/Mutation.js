86054a0d9c83c60088719fd6ede1d19c3ea87c1b
Patrick <patrick@responsive.co.za>
2019-02-28 15:17:46 +0200

    Finished backend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    });
+    return item;
+  }
+};
 
 module.exports = Mutations;
