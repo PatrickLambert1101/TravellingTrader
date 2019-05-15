68f6f8fe7966ebecc619273756a027467c520dc8
Patrick <patrick@responsive.co.za>
2019-05-15 07:33:39 +0200

    Initial Commit

---
+++ frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import PleaseSignIn from '../components/PleaseSignIn';
+import CreateItem from '../components/CreateItem';
+const Sell = props => (
+  <div>
+    <PleaseSignIn>
+      <CreateItem />
+    </PleaseSignIn>
+  </div>
+);
+
+export default Sell;
